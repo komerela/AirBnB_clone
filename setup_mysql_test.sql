398b7c083dd9a12e51aab5c76a6ab04a403a42ee
komerela <kmwongera@icloud.com>
2019-08-13 23:21:21 +0000

    script that prepares a MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Script that prepares mysql server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON *.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+FLUSH PRIVILEGES;
