c6ded3519844f9aaf30d1908820b63adedb1d648
komerela <kmwongera@icloud.com>
2019-08-16 05:59:19 +0000

    Pep8 formatting changes

--- models/city.py
+++ models/city.py
@@ -9,18 +9,17 @@ from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
+    """
+    This is the class for City
     """
     __tablename__ = 'cities'
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
 
-        name = "Column(String(128), nullable=False)"
-        state_id = Column(String(60), nullable=False, ForeignKey('states.id')
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         state = relationship("State")
         places = relationship("Place", cascade="all, delete-orphan")
 
