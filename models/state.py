4aef00beb3aadaac417eb0026d551ff145c87a5f
komerela <kmwongera@icloud.com>
2019-08-16 06:32:43 +0000

    State updated

--- models/state.py
+++ models/state.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from models.base_model import BaseModel
+'''
+    Implementation of the State class with new updates
+'''
+import models
+import sqlalchemy
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
-class State(BaseModel):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    name = ""
+class State(BaseModel, Base):
+    '''
+        Implementation for the State.
+    '''
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete-orphan")
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            instance_list = []
+            for key, obj in models.storage.all().items():
+                if obj.__class__.__name__ == 'City':
+                    if obj.state_id == self.id:
+                        instance_list.append(obj)
+            return instance_list
